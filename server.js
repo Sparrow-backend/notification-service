14761c45f64c30a2b77eab73a1242ed8668cb667
sliitguy <it23259416@my.sliit.lk>
2025-10-03 22:26:25 +0530

    updated basic files

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const app = require('./src/app'); // your app.js
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8005;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// MongoDB connection events
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is ready!!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting with MongoDB:', err);
+});
+
+// Start server
+async function startServer() {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+
+    // Start listening
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}...`);
+    });
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1); // exit process if DB connection fails
+  }
+}
+
+startServer();
---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors({
+    origin: [
+        'https://sparrow.nivakaran.dev',
+        'http://localhost:3000',
+        'http://nivakaran.dev'
+    ]
+}))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "Sparrow: Parcel Service"})
+})
+
+app.get('/health', (req, res) => {
+    res.json({message: "Parcel Service is running.."})
+})
+
+
+module.exports = app
